d10060cdb0b7be33b7fefa471a73f2f3d9e3b531
fxx <394967886@qq.com>
2024-12-31 13:41:26 +1300

    add login register api

---
+++ Demo.Contracts/Authentication/AutenticationResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Contracts.Authentication
+{
+    public record AutenticationResponse(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token);
+
+}
